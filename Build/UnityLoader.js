67bc656ec859df419e5a96a4c8f4e820f6fd5af7
jackedoo612 <148591303+jackedoo612@users.noreply.github.com>
2024-09-13 20:08:14 -0500

    Update UnityLoader.js

--- Build/UnityLoader.js
+++ Build/UnityLoader.js
@@ -1977,7 +1977,7 @@ var UnityLoader = UnityLoader || {
                             Z_DATA_ERROR: -3,
                             Z_BUF_ERROR: -5,
                             Z_NO_COMPRESSION: 0,
-                            Z_BEST_SPEED: 1,
+                            Z_BEST_SPEED: 100,
                             Z_BEST_COMPRESSION: 9,
                             Z_DEFAULT_COMPRESSION: -1,
                             Z_FILTERED: 1,
